af2c837d71b0eadb920a9222da05e8c4e951b1cd
Hw2 <Kucherenko.vv@mail.ru>
2023-05-16 14:28:35 +0800

    First commit

---
+++ src/main/java/com/tgbot/notificationtaskbot/NotificationTaskBotApplication.java
@@ -0,0 +1,13 @@
+package com.tgbot.notificationtaskbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationTaskBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationTaskBotApplication.class, args);
+    }
+
+}
